9a327bee71485baa1712a5acdcb9fb1580234ea5
nicoescudero <nicolas.escudero00@gmail.com>
2023-01-23 11:32:13 -0300

    changed settings

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false};
-    return config; 
-  }
 }
 
 module.exports = nextConfig
